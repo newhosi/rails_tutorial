fb430e924e4d0cb2912764aa611666d31b1fc877
SAEBYEOL KIM <saebyeolkim@SAEBYEOLui-MacBookPro.local>
2024-11-30 18:14:54 +0900

    chore: fix lint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # request URL /path will be mapped to the Path action in the Static Pages controller
 Rails.application.routes.draw do
   root "static_pages#home" # "/" is the root path
-  get '/help', to: 'static_pages#help'
-  get '/about', to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
+  get "/help", to: "static_pages#help"
+  get "/about", to: "static_pages#about"
+  get "/contact", to: "static_pages#contact"
 end
