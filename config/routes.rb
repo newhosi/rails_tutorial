5775db94c1e1de3dfbaf8d72242de02440e3df5b
saebyeol <newhosi27@gmail.com>
2024-12-22 18:05:40 +0900

    feat: add login path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 # request URL /path will be mapped to the Path action in the Static Pages controller
 Rails.application.routes.draw do
+  # static
   root "static_pages#home" # "/" is the root path
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
+  
+  # users
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
   resources :users
+
+  ## sessions
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
