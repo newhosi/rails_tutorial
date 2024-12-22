1f48932b14a3b7d54187534c0e46d36144d9c718
saebyeol <newhosi27@gmail.com>
2024-12-22 18:07:41 +0900

    chore: add login form

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get "/help", to: "static_pages#help"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
-  
+
   # users
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
