059f62eb9e3e0eeebfe1269652fbec1faf2ce3f9
saebyeol <newhosi27@gmail.com>
2025-01-02 19:58:28 +0900

    test: advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -25,7 +25,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template "users/show"
     assert flash.empty?
     assert_select "a[href=?]", login_path, count: 0
-    # assert_select "a[href=?]", logout_path, count: 1
+    assert_select "a[href=?]", logout_path, count: 1
     assert_select "a[href=?]", user_path(@user), count: 1
   end
 
@@ -34,20 +34,21 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_template "sessions/new"
     post login_path, params: { session: { email: @user.email, password: "password" } }
     assert is_logged_in?
-    assert_response :redirect
     follow_redirect!
     assert_template "users/show"
     assert flash.empty?
     assert_select "a[href=?]", login_path, count: 0
-    # assert_select "a[href=?]", logout_path, count: 1
+    assert_select "a[href=?]", logout_path, count: 1
     assert_select "a[href=?]", user_path(@user), count: 1
 
     delete logout_path
     assert_not is_logged_in?
-    assert_response :redirect
+    follow_redirect!
+
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path, count: 1
-    # assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
