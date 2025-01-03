2891eae4d78607e23777b7cf0c478f0d334b7a14
saebyeol <newhosi27@gmail.com>
2025-01-03 11:47:42 +0900

    chore: add friendly forwarding to users edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [ :edit, :update ]
-  before_action :correct_user, only: [ :edit, :update ]
+  before_action :correct_user,   only: [ :edit, :update ]
 
   def show
     @user = User.find(params[:id])
@@ -43,6 +43,7 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
