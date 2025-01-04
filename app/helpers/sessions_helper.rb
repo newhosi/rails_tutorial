7c71f3c75dc657955f16a73bb0cffeb59ec7636f
saebyeol <newhosi27@gmail.com>
2025-01-04 13:38:43 +0900

    chore: add request.head? to sessions_helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -47,7 +47,7 @@ module SessionsHelper
   end
 
   def store_location
-    session[:forwarding_url] = request.original_url if request.get?
+    session[:forwarding_url] = request.original_url if request.get? || request.head?
     puts "session[:forwarding_url] = #{session[:forwarding_url]}"
   end
 end
