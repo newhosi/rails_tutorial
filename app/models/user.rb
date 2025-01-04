9b91d4ac908df8f285481701597cde70dfd65f70
saebyeol <newhosi27@gmail.com>
2025-01-04 18:41:48 +0900

    feat: add account activation mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,24 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  attr_accessor :remember_token, :activation_token
+  has_secure_password
+  before_create :create_activation_digest
+  before_save   :downcase_email
 
-  before_save { self.email = email.downcase }
   validates :name,
             presence: true,
             length: { maximum: 50 }
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email,
             presence: true,
             length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
 
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :password,
+             presence: true,
+             length: { minimum: 6 },
+             allow_nil: true
 
   def remember
     self.remember_token = User.new_token
@@ -40,4 +44,14 @@ class User < ApplicationRecord
       SecureRandom.urlsafe_base64
     end
   end
+
+  private
+    def downcase_email
+      self.email.downcase!
+    end
+
+    def create_activation_digest
+      activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
