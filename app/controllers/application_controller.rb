e8489d5ac5d308829ea6d4a37858625a8ba9fa56
saebyeol <newhosi27@gmail.com>
2025-01-11 22:39:28 +0900

    refac: move check logged in user to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,13 @@ class ApplicationController < ActionController::Base
   include SessionsHelper
 
   include Pagy::Backend
+
+  private
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
