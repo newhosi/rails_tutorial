21a569a55934b9bac60b2a5757ca4b8b7402447a
saebyeol <newhosi27@gmail.com>
2025-01-11 22:22:08 +0900

    test: list user microposts

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,10 +6,9 @@ Minitest::Reporters.use!
 
 module ActiveSupport
   class TestCase
-    # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
-
     fixtures :all
+    include Pagy::Backend
 
     def is_logged_in?
       !session[:user_id].nil?
