82ff619b7b64cef41310b1acd11e8d0c26d55069
saebyeol <newhosi27@gmail.com>
2025-01-12 21:07:23 +0900

    seed: add relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,4 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# users
 User.create!(name:                  "example-user",
              email:                 "example-user@example.com",
              password:              "password",
@@ -27,8 +19,17 @@ User.create!(name:                  "example-user",
     activated:             true,
     activated_at:          Time.zone.now)
   3.times do |n|
+    # microposts
     user.microposts.create!(
       content: Faker::Lorem.sentence(word_count: 5)
     )
   end
 end
+
+# relationships
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
