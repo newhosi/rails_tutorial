cf730b97593b00641621ec8d158c079408bbfce0
saebyeol <newhosi27@gmail.com>
2025-01-13 16:51:00 +0900

    feat: add following and followers lists

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [ :index, :edit, :update, :destroy ]
+  before_action :logged_in_user, only: [ :index, :edit, :update, :destroy, :followers, :following ]
   before_action :correct_user,   only: [ :edit, :update ]
   before_action :admin_user,     only: [ :destroy ]
 
@@ -15,6 +15,22 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
+  end
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following
+    @pagy, @users_pagy = pagy(@user.following)
+    render "show_follow"
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers
+    @pagy, @users_pagy = pagy(@user.followers)
+    render "show_follow"
   end
 
   def create
