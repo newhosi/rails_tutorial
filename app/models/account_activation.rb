c1d0b040e016478c8333feee1cba6468ec57124f
saebyeol <newhosi27@gmail.com>
2025-02-02 00:53:06 +0900

    fix: account activation and refac user model

--- app/models/account_activation.rb
+++ app/models/account_activation.rb
@@ -1,3 +1,14 @@
 class AccountActivation < ApplicationRecord
+  self.primary_key = "user_id"
   belongs_to :user
+
+  def authenticated?(token)
+    digest = send("activation_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -55,15 +55,15 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
-  def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
-    return false if digest.nil?
-    BCrypt::Password.new(digest).is_password?(token)
-  end
+  # def authenticated?(attribute, token)
+  #   digest = send("#{attribute}_digest")
+  #   return false if digest.nil?
+  #   BCrypt::Password.new(digest).is_password?(token)
+  # end
 
-  def activate
-    update_columns(activated: true, activated_at: Time.zone.now)
-  end
+  # def activate
+  #   update_columns(activated: true, activated_at: Time.zone.now)
+  # end
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
