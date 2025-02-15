d8deb04e5b9dc243d9da94895d419cb84e02d5f9
saebyeol <newhosi27@gmail.com>
2025-02-15 21:22:43 +0900

    chore: add log in as helper to rspec

--- spec/support/test_helpers.rb
+++ spec/support/test_helpers.rb
@@ -3,4 +3,10 @@ module TestHelpers
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+
+  def log_in_as(user, password: "password", remember_me: "1")
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
