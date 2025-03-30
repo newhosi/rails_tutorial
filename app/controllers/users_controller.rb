7abe714340bdf55a4ee33517e2f7b2318175255f
saebyeol <newhosi27@gmail.com>
2025-03-30 22:55:51 +0900

    style: update ui layout on users#show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @tab = params[:tab] || "posts"
     @pagy, @microposts = pagy(@user.microposts)
     redirect_to root_url and return unless @user.activated?
   end
