1749ab6edee7e542da5db18ef15e910ee5b01add
saebyeol <newhosi27@gmail.com>
2025-03-30 22:58:00 +0900

    style: add micropost detail view

---
+++ app/javascript/controllers/modal_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus";
+
+// Connects to data-controller="modal"
+export default class extends Controller {
+  static targets = ["panel"];
+
+  open(event) {
+    event.preventDefault();
+    this.panelTarget.closest("[data-modal-id]").classList.remove("hidden");
+  }
+
+  close(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.panelTarget.closest("[data-modal-id]").classList.add("hidden");
+  }
+
+  stop(event) {
+    event.stopPropagation();
+  }
+}
